5d983567abbc22c7857b9994434a8d5bc593d1da
Tim <timhuang814@gmail.com>
2020-09-23 19:06:25 -0400

    Radar plot
    -installed chartjs
    -implemented rdar graph in app.js

--- unethical-corp/src/App.js
+++ unethical-corp/src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Chart from 'chart.js';
+
+// const value = 1.6
+// const chartConfig = {
+//   type: 'radar',
+//   data: {
+//     labels: ["Safety", "Privacy", "Reputation", "Salary"],
+//     datasets: [{
+//       label: "Student A",
+//       backgroundColor: colorLimit(value),
+//       data: [65, 75, 70, 80]
+//     }]
+//   },
+
+//   options: {
+//     scale: {
+//       ticks: {
+//         beginAtZero: true,
+//         min: 0
+//       }
+//     }
+//   }
+// };
+
 
 function App() {
+
+
+
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+  // state for input
+  const [input, setInput] = useState(0);
+  const chartConfig = onChartChange(input)
+
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chart(chartContainer.current, chartConfig);
+      setChartInstance(newChartInstance);
+    }
+  }, [chartContainer]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas
+        ref={chartContainer}
+      />
+      <form>
+        <label>
+          Input:
+          <input type="text" value={input} onChange={(event) => { setInput(event.target.value) }} />
+        </label>
+
+      </form>
+
     </div>
   );
 }
 
+function colorLimit(value) {
+  if (value < 1.5) {
+    return "rgba(255, 0, 0, 0.2)"
+  }
+  else if (value < 2.5) {
+    return "rgba(255, 255, 0, 0.2)"
+  }
+  else {
+    return "rgba(0, 128, 0, 0.2)"
+  }
+}
+
+function onChartChange(input) {
+  return {
+    type: 'radar',
+    data: {
+      labels: ["Safety", "Privacy", "Reputation", "Salary"],
+      datasets: [{
+        label: "Student A",
+        backgroundColor: colorLimit(input),
+        data: [65, 75, 70, 80]
+      }]
+    },
+
+    options: {
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          min: 0,
+          display: false
+        }
+      }
+    }
+  };
+}
 export default App;
+
+
