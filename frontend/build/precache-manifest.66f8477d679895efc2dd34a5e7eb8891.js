7b984fd0d933943d954b35fddd3a050f143ba87d
Song Chen <43616275+songchen1999@users.noreply.github.com>
2020-10-10 21:09:29 -0400

    more restore

---
+++ frontend/build/precache-manifest.66f8477d679895efc2dd34a5e7eb8891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cd12826e0b1560eb56fd73f8f35228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fac7c5e853cf1f35d98",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d996a4080c236b6713b9",
+    "url": "/static/css/main.57cfaafc.chunk.css"
+  },
+  {
+    "revision": "3fac7c5e853cf1f35d98",
+    "url": "/static/js/2.b6101860.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.b6101860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d996a4080c236b6713b9",
+    "url": "/static/js/main.714ec9a0.chunk.js"
+  },
+  {
+    "revision": "dc3903c1cbbbe7c15fd0",
+    "url": "/static/js/runtime-main.6b445ecb.js"
+  }
+]);
