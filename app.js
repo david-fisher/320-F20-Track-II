62557613a0f7176f039afda133d7919581234c2b
Kunal Goyal <kgoyalwork@gmail.com>
2020-10-11 23:05:03 +0530

    added route example

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const db = require('./queries')
+const router = express.Router()
 
 //const scenarios = [{id : 1, name : 'Walmart Tech Lab', desc : '', stakeholders: [{name: 'Professor Ron'}]},
 //{id : 2, name : 'Uber Eats', desc : '', stakeholders: [{name: 'Professor Sarah'}, {name: 'Company Lawyer'}]}]
@@ -17,16 +18,53 @@ app.use(
 )
 
 
-app.get('/', (req, res) => {
+router.use(function(req, res, next) {
+    // do logging
+    console.log('API is running')
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+router.get('/', (req, res) => {
     res.json({info: 'This is the API'})
+    next()
 })
 
 
-app.get('/scenarios', db.getScenarios)
-app.get('/scenarios/:id', db.getScenraioById)
-app.post('/scenarios/', db.createScenario)
-app.put('/scenarios/:id', db.updateScenario)
-app.delete('/scenarios/:id', db.deleteScenario)
+router.route('/scenarios')
+
+    .get(function(req, res){
+        // studentID = req.body
+        // scenarios = db.getScenarios(studentID)
+        scenarios = db.getScenarios()
+        res.json(scenarios)
+    })  
+//use this code as a template
+
+
+router.route('/scenarios/scenarioName')
+//add get functions for this route
+
+
+//create routes as needed
+
+
+
+router.route('/scenarios/scenarioName/initreflection')
+
+    .post()
+
+
+
+
+
+
+
+// app.get('/scenarios', db.getScenarios)
+// app.get('/scenarios/:id', db.getScenraioById)
+// app.post('/scenarios/', db.createScenario)
+// app.put('/scenarios/:id', db.updateScenario)
+// app.delete('/scenarios/:id', db.deleteScenario)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
