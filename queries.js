e54467595fd6c9ea2e1491bd2da7ae600d909556
MarcotteCaleb <70389777+MarcotteCaleb@users.noreply.github.com>
2020-10-26 20:42:31 -0400

    New function format

--- queries.js
+++ queries.js
@@ -13,132 +13,132 @@ const pool = new Pool({
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0
 })
-
-async function getIntro(scenarioID){
-    try{
-        let thisQuery= 'select prompt.prompt from pages, prompt where pages.id = prompt.page_id and pages.order = 1 and scenario_id = $1'
-        let results= await pool.query(thisQuery, [scenarioID])
-        return results.rows[0].prompt
+/*
+function getScenarios(studentID, callback){
+  pool.query('SELECT id, name, description FROM scenarios ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
     }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+  callback(results.rows)  
+  })  
+}
+*/
+function getIntro(scenarioID, callback){
+    let thisQuery= 'select prompt.prompt from pages, prompt where pages.id = prompt.page_id and pages.order = 1 and scenario_id = $1'
+    pool.query(thisQuery, [scenarioID], (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    })  
 }
 
-async function getIntroResponse(studentID, scenarioID){
-    try{
-        let thisQuery= 'select prompt_response.response from prompt_response, response, submissions, pages where pages.order=1 and response.page_id=pages.id and response.id= prompt_response.id and response.submission_id=submissions.id and submissions.user_id = ' + studentID + 'and pages.scenario_id =' + scenarioID
-        let results= await pool.query(thisQuery)
-        return results.rows[0].response
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+function getIntroResponse(studentID, scenarioID){
+    let thisQuery= 'select prompt_response.response from prompt_response, response, submissions, pages where pages.order=1 and response.page_id=pages.id and response.id= prompt_response.id and response.submission_id=submissions.id and submissions.user_id = ' + studentID + 'and pages.scenario_id =' + scenarioID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
-async function getMiddleResponse(studentID, scenarioID){
-    try{
-        let thisQuery= 'select prompt_response.response from prompt_response, response, submissions, pages where pages.order=3 and response.page_id=pages.id and response.id= prompt_response.id and response.submission_id=submissions.id and submissions.user_id = ' + studentID + 'and pages.scenario_id =' + scenarioID
-        let results= await pool.query(thisQuery)
-        return results.rows[0].response
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+function getMiddleResponse(studentID, scenarioID){
+    let thisQuery= 'select prompt_response.response from prompt_response, response, submissions, pages where pages.order=3 and response.page_id=pages.id and response.id= prompt_response.id and response.submission_id=submissions.id and submissions.user_id = ' + studentID + 'and pages.scenario_id =' + scenarioID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
-async function getFinalResponse(studentID, scenarioID){
-    try{
-        let thisQuery= 'select prompt_response.response from prompt_response, response, submissions, pages where pages.order=5 and response.page_id=pages.id and response.id= prompt_response.id and response.submission_id=submissions.id and submissions.user_id = ' + studentID + 'and pages.scenario_id =' + scenarioID
-        let results= await pool.query(thisQuery)
-        return results.rows[0].response
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+function getFinalResponse(studentID, scenarioID){
+    let thisQuery= 'select prompt_response.response from prompt_response, response, submissions, pages where pages.order=5 and response.page_id=pages.id and response.id= prompt_response.id and response.submission_id=submissions.id and submissions.user_id = ' + studentID + 'and pages.scenario_id =' + scenarioID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
 //Get the names, ids, and descriptions of each stakeholder in a scenario
-async function getStakeholders(scenarioID){
-    try{
-        let thisQuery= 'select stakeholders.name, stakeholders.id, stakeholders.description from stakeholders where stakeholders.scenario_id =' + scenarioID
-        let results= await pool.query(thisQuery)
-        return results.rows;
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
-    
+function getStakeholders(scenarioID){
+    let thisQuery= 'select stakeholders.name, stakeholders.id, stakeholders.description from stakeholders where stakeholders.scenario_id =' + scenarioID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
-async function getName(userID){
-    try{
-        let thisQuery= 'select users.full_name from users where id =' + userID
-        let results= await pool.query(thisQuery)
-        return results.rows[0].full_name;
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+function getName(userID){
+    let thisQuery= 'select users.full_name from users where id =' + userID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
+}
+
+function getCourseInfo(courseID){
+    let thisQuery= 'select webpage, name, semester from courses where id =' + courseID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
 async function getCourseInfo(courseID){
-    try{
-        let thisQuery= 'select webpage, name, semester from courses where id =' + courseID
-        let results= await pool.query(thisQuery)
-        return results.rows;
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+    let thisQuery= 'select webpage, name, semester from courses where id =' + courseID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
-async function getCourseInfo(courseID){
-    try{
-        let thisQuery= 'select webpage, name, semester from courses where id =' + courseID
-        let results= await pool.query(thisQuery)
-        return results.rows;
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+function getInstructorInfo(instructorID){
+    let thisQuery= 'select full_name, email, webpage, course_id from instructs, users where instructs.id =' + instructorID + 'and users.id='+ instructorID
+    pool.query(thisQuery, (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
-async function getInstructorInfo(instructorID){
-    try{
-        let thisQuery= 'select full_name, email, webpage, course_id from instructs, users where instructs.id =' + instructorID + 'and users.id='+ instructorID
-        let results= await pool.query(thisQuery)
-        return results.rows;
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-    }
+function addUser(fullName, email){
+    let thisQuery= 'insert into users(full_name, email) values ($1 , $2)';
+    pool.query(thisQuery, [fullName, email], (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
-async function addUser(fullName, email){
-    try{
-        let thisQuery= 'insert into users(full_name, email) values ($1 , $2)';
-        let results= await pool.query(thisQuery, [fullName, email])
-        return 1
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-        return -1
-    }
+function addCourse(coursePage, courseName, semester){
+    let thisQuery= 'insert into courses(webpage, name, semester) values ($1, $2, $3)';
+    pool.query(thisQuery, [coursePage, courseName, semester], (error,results) => {
+        if (error) {
+            throw error
+        }
+        callback(results.rows)
+    }) 
 }
 
-async function addCourse(coursePage, courseName, semester){
-    try{
-        let thisQuery= 'insert into courses(webpage, name, semester) values ($1, $2, $3)';
-        let results= await pool.query(thisQuery, [coursePage, courseName, semester])
-        return 1
-    }
-    catch (ex){
-        console.log("FAILURE: " + ex)
-        return -1
-    }
+function cb(results){
+    console.log(results.rows)
 }
 
-//getCourseInfo(1).then(x => console.log(x))
+getIntro(1, cb)
 //getInstructorInfo(4).then(x => console.log(x[0]));
 //addCourse('New Course','CS305','F2020').then(x => console.log(x));
 pool.end()
+
