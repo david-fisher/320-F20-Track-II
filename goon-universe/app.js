f59ba8ad8d8d7f9f7eff8569a3cee5c4b50f409d
Song Chen <43616275+songchen1999@users.noreply.github.com>
2020-11-02 21:00:52 -0500

    set up local db environment and change server port number

--- goon-universe/app.js
+++ goon-universe/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 5000
 const db = require('./queries')
 const isnumber = require('is-number')
 
--- goon-universe/queries.js
+++ goon-universe/queries.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool
 require("dotenv").config()
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
 })
 
 function getScenarios(studentID, callback){
