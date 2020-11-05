96d36e38c6d09ca53088967185c6dac295fd7d2a
sspathak <suraj.s.pathak@gmail.com>
2020-11-05 14:20:44 +0400

    updated example data

--- database/insert_example_data.sql
+++ database/insert_example_data.sql
@@ -23,16 +23,19 @@ insert into partof values(1,1);
 INSERT INTO pages VALUES(DEFAULT, 1, 'PLAIN', 1);
 INSERT INTO plain_page VALUES(1, 'plain_page');
 
-INSERT INTO pages VALUES(DEFAULT, 2, 'CONV', 1);
-INSERT INTO conversation_task VALUES(2, 'conversation_task: <obj>');
+INSERT INTO pages VALUES(DEFAULT, 2, 'PRMPT', 1);
+INSERT INTO prompt VALUES(2, 'Initial reflection prompt...')
+
+INSERT INTO pages VALUES(DEFAULT, 3, 'CONV', 1);
+INSERT INTO conversation_task VALUES(3, 'conversation_task: <obj>');
 INSERT INTO stakeholders VALUES(DEFAULT, 'Sherlock Holmes', 'Detective', '<conversation text>', 1, 2);
 
-INSERT INTO pages VALUES(DEFAULT, 3, 'PRMPT', 1);
-INSERT INTO prompt VALUES(3, 'prompt: middle reflection');
+INSERT INTO pages VALUES(DEFAULT, 4, 'PRMPT', 1);
+INSERT INTO prompt VALUES(4, 'prompt: middle reflection');
 
-INSERT INTO pages VALUES(DEFAULT, 4, 'MCQ', 1);
-INSERT INTO mcq VALUES(4, 'MCQ: <obj>');
-INSERT INTO question VALUES(DEFAULT, '(S1) MCQ Question 1: <text>', 4);
+INSERT INTO pages VALUES(DEFAULT, 5, 'MCQ', 1);
+INSERT INTO mcq VALUES(5 'MCQ: <obj>');
+INSERT INTO question VALUES(DEFAULT, '(S1) MCQ Question 1: <text>', 5);
 INSERT INTO mcq_option VALUES(DEFAULT, '(S1) MCQ (1) option A', 1);
 INSERT INTO mcq_option VALUES(DEFAULT, '(S1) MCQ (1) option B', 1);
 INSERT INTO mcq_option VALUES(DEFAULT, '(S1) MCQ (1) option C', 1);
@@ -44,35 +47,35 @@ INSERT INTO mcq_option VALUES(DEFAULT, '(S1) MCQ (2) option B', 2);
 INSERT INTO mcq_option VALUES(DEFAULT, '(S1) MCQ (2) option C', 2);
 
 
-INSERT INTO pages VALUES(DEFAULT, 5, 'PRMPT', 1);
-INSERT INTO prompt VALUES(5, 'prompt: final reflection');
+INSERT INTO pages VALUES(DEFAULT, 6, 'PRMPT', 1);
+INSERT INTO prompt VALUES(6, 'prompt: final reflection');
 
-INSERT INTO pages VALUES(DEFAULT, 6, 'PLAIN', 1);
-INSERT INTO plain_page VALUES(6, 'Page: results will be available in one week');
+INSERT INTO pages VALUES(DEFAULT, 7, 'PLAIN', 1);
+INSERT INTO plain_page VALUES(7, 'Page: results will be available in one week');
 
 
-INSERT INTO submissions VALUES(DEFAULT, 1, 1, '2020-10-10 10:10:00');
-INSERT INTO response VALUES(DEFAULT, 1, 1, '2020-10-10 10:10:00');
+INSERT INTO submissions VALUES(DEFAULT, 1, 2, '2020-10-10 10:10:00');
+INSERT INTO response VALUES(DEFAULT, 1, 2, '2020-10-10 10:10:00');
 INSERT INTO prompt_response VALUES(1, 'John Doe''s response to initial reflection');
 
-INSERT INTO response VALUES(DEFAULT, 1, 3, '2020-10-11 10:10:00');
+INSERT INTO response VALUES(DEFAULT, 1, 4, '2020-10-11 10:10:00');
 INSERT INTO prompt_response VALUES(2, 'John Doe''s response to middle reflection');
 
-INSERT INTO response VALUES(DEFAULT, 1, 5, '2020-10-12 10:10:00');
+INSERT INTO response VALUES(DEFAULT, 1, 6, '2020-10-12 10:10:00');
 INSERT INTO prompt_response VALUES(3,'John Doe''s response to final reflection');
 
-INSERT INTO response VALUES(DEFAULT, 1, 2, '2020-10-10 10:10:00');
+INSERT INTO response VALUES(DEFAULT, 1, 3, '2020-10-10 10:10:00');
 INSERT INTO conversation_choices VALUES(4, 1);
 
 
 INSERT INTO submissions VALUES(DEFAULT, 2, 1, '2020-10-10 10:10:00');
-INSERT INTO response VALUES(DEFAULT, 2, 1, '2020-10-11 09:10:00');
+INSERT INTO response VALUES(DEFAULT, 2, 2, '2020-10-11 09:10:00');
 INSERT INTO prompt_response VALUES(5, 'Jane Doe''s response to Initial reflection');
 
-INSERT INTO response VALUES(DEFAULT, 2, 3, '2020-10-11 09:10:00');
+INSERT INTO response VALUES(DEFAULT, 2, 4, '2020-10-11 09:10:00');
 INSERT INTO prompt_response VALUES(6, 'Jane Doe''s response to middle reflection');
 
-INSERT INTO response VALUES(DEFAULT, 2, 4, '2020-10-11 09:10:00');
+INSERT INTO response VALUES(DEFAULT, 2, 5, '2020-10-11 09:10:00');
 INSERT INTO mcq_response VALUES(7, 1, 3);
 
 
