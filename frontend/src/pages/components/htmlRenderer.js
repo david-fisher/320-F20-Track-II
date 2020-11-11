fdc5d6e869ec7ad4d31436b0affa3b31897e59db
Dylan Nelson <dylan.rae.nelson@gmail.com>
2020-11-11 13:52:30 -0500

    Sanitize html input to try to prevent XSS attacks

--- frontend/src/pages/components/htmlRenderer.js
+++ frontend/src/pages/components/htmlRenderer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 
-const HTMLRenderer = ({html}) => {return (<div className={'sun-editor-editable'} dangerouslySetInnerHTML={{__html: html}}></div>)}
+const HTMLRenderer = ({html}) => {
+    const sanitizedHTML = html.replace(/onerror=/, '').replace(/<a.*onmouseover=.*>/, '<a>');
+    return (<div className={'sun-editor-editable'} dangerouslySetInnerHTML={{__html: sanitizedHTML}}></div>);
+}
 
 export default HTMLRenderer;
