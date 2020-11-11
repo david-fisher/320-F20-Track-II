cd06a9b67838df32d2bc55fd00a420d2903276f0
Dylan Nelson <dylan.rae.nelson@gmail.com>
2020-11-11 13:56:13 -0500

    Fix typo

--- frontend/src/pages/components/htmlRenderer.js
+++ frontend/src/pages/components/htmlRenderer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const HTMLRenderer = ({html}) => {
-    const sanitizedHTML = html.replace(/onerror=/, '').replace(/<a.*onmouseover=.*>/, '<a>');
+    const sanitizedHTML = html.replace(/onerror=/, '').replace(/<a .*onmouseover=.*>/, '<a>');
     return (<div className={'sun-editor-editable'} dangerouslySetInnerHTML={{__html: sanitizedHTML}}></div>);
 }
 
